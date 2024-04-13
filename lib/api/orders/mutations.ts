6df707682f51dbcca0e6b7bf9c4d750951b4b91e
zbun-karina <vt211_zks@student.ztu.edu.ua>
2024-04-13 17:26:54 +0300

    chore: add orders module

---
+++ lib/api/orders/mutations.ts
@@ -0,0 +1,53 @@
+import { db } from '@/lib/db';
+import {
+  insertOrderSchema,
+  NewOrderParams,
+  OrderId,
+  orderIdSchema,
+  UpdateOrderParams,
+  updateOrderSchema,
+} from '@/lib/db/schema/orders';
+import { getUserAuth } from '@/lib/auth/utils';
+
+export const createOrder = async (order: NewOrderParams) => {
+  const { session } = await getUserAuth();
+  const newOrder = insertOrderSchema.parse({ ...order, userId: session?.user.id! });
+  try {
+    const o = await db.order.create({ data: newOrder });
+    return { order: o };
+  } catch (err) {
+    const message = (err as Error).message ?? 'Error, please try again';
+    console.error(message);
+    throw Error(message);
+  }
+};
+
+export const updateOrder = async (id: OrderId, order: UpdateOrderParams) => {
+  const { session } = await getUserAuth();
+  const { id: orderId } = orderIdSchema.parse({ id });
+  const newOrder = updateOrderSchema.parse({ ...order, userId: session?.user.id! });
+  try {
+    const o = await db.order.update({
+      where: { id: orderId, userId: session?.user.id! },
+      data: newOrder,
+    });
+    return { order: o };
+  } catch (err) {
+    const message = (err as Error).message ?? 'Error, please try again';
+    console.error(message);
+    throw Error(message);
+  }
+};
+
+export const deleteOrder = async (id: OrderId) => {
+  const { session } = await getUserAuth();
+  const { id: orderId } = orderIdSchema.parse({ id });
+  try {
+    const o = await db.order.delete({ where: { id: orderId, userId: session?.user.id! } });
+    return { order: o };
+  } catch (err) {
+    const message = (err as Error).message ?? 'Error, please try again';
+    console.error(message);
+    throw Error(message);
+  }
+};
---
+++ prisma/zod/order.ts
@@ -0,0 +1,25 @@
+import * as z from "zod"
+import { CompleteUser, relatedUserSchema } from "./index"
+
+export const orderSchema = z.object({
+  id: z.string(),
+  number: z.number().int(),
+  price: z.number(),
+  notes: z.string().nullish(),
+  userId: z.string(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+})
+
+export interface CompleteOrder extends z.infer<typeof orderSchema> {
+  user: CompleteUser
+}
+
+/**
+ * relatedOrderSchema contains all relations on your model in addition to the scalars
+ *
+ * NOTE: Lazy required in case of potential circular dependencies within schema
+ */
+export const relatedOrderSchema: z.ZodSchema<CompleteOrder> = z.lazy(() => orderSchema.extend({
+  user: relatedUserSchema,
+}))
