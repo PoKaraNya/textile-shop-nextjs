7a4c42e15f879f5cb7bfba0cff96ef472e2271b2
zbun-karina <vt211_zks@student.ztu.edu.ua>
2024-04-14 00:28:45 +0300

    feat: improve db seed and reset

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,15 @@
-const { PrismaClient: PrismaClientSeed } = require('@prisma/client');
-const { faker } = require('@faker-js/faker');
+import { faker } from '@faker-js/faker';
+import { PrismaClient } from '@prisma/client';
+import { getRandomElement, getRandomNumber } from '../lib/utils';
 
-const prismaSeed = new PrismaClientSeed();
+const prismaSeed = new PrismaClient();
 
 const generateUsers = async (count: number) => {
   for (let i = 0; i < count; i++) {
     const email = faker.internet.email();
     const name = faker.person.firstName();
-    await prismaSeed.user.upsert({
-      where: { email },
-      update: {},
-      create: {
+    await prismaSeed.user.create({
+      data: {
         email,
         name,
       },
@@ -19,15 +18,21 @@ const generateUsers = async (count: number) => {
 };
 
 const generateProducts = async (count: number) => {
+  const categories = await prismaSeed.category.findMany();
+
   for (let i = 0; i < count; i++) {
-    const title = faker.commerce.product();
-    await prismaSeed.product.upsert({
-      update: {},
-      where: { title },
-      create: {
+    const title = `${faker.commerce.product()} ${getRandomNumber(10000)}`;
+    await prismaSeed.product.create({
+      data: {
         title,
         description: faker.commerce.productDescription(),
-        price: Math.trunc(Math.random() * 10000),
+        photo: faker.image.urlPicsumPhotos(),
+        price: getRandomNumber(10000),
+        category: {
+          connect: {
+            id: getRandomElement(categories)?.id,
+          },
+        },
       },
     });
   }
@@ -35,13 +40,12 @@ const generateProducts = async (count: number) => {
 
 const generateCategory = async (count: number) => {
   for (let i = 0; i < count; i++) {
-    const title = `Category ${Math.trunc(Math.random() * 100000)}`;
-    await prismaSeed.category.upsert({
-      update: {},
-      where: { title },
-      create: {
+    const title = `Category ${getRandomNumber(10000)}`;
+    await prismaSeed.category.create({
+      data: {
         title,
         description: faker.commerce.productDescription(),
+        photo: faker.image.urlPicsumPhotos(),
       },
     });
   }
@@ -49,8 +53,8 @@ const generateCategory = async (count: number) => {
 
 async function seedDb() {
   await generateUsers(50);
-  await generateProducts(50);
-  await generateCategory(50);
+  await generateCategory(10);
+  await generateProducts(100);
 }
 
 seedDb()
