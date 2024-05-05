50f8e5e656d0ef570d59c3b5b368c1238092999a
zbun-karina <vt211_zks@student.ztu.edu.ua>
2024-05-05 17:25:05 +0300

    feat: add seed for favorites

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -82,13 +82,26 @@ const generateCarts = async (count: number) => {
   }));
 };
 
+const generateFavorites = async (count: number) => {
+  const products = await db.product.findMany();
+  const users = await db.user.findMany();
+
+  return timesAsync(count, async () => db.favorite.create({
+    data: {
+      product: connectRandom(products),
+      user: connectRandom(users),
+    },
+  }));
+};
+
 export async function seedDb() {
-  await generateUsers(50);
+  await generateUsers(20);
   await generateCategories(10);
   await generateProducts(100); // category
   await generateOrders(50); // user, products
   await generateFeedbacks(500); // user, products
-  await generateCarts(100); // user, products
+  await generateCarts(150); // user, products
+  await generateFavorites(150); // user, products
 }
 
 seedDb()
